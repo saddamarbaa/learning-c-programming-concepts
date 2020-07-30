057e5ef63262e54fa76d15ab6fc36db21dc3d51c
saddamarbaa <51326421+saddamarbaa@users.noreply.github.com>
2020-07-31 06:02:04 +0700

    C Scope of Variables in Local , Global variables , Const , Automati…

--- C Variables  Scope of Variables.c
+++ C Variables  Scope of Variables.c
@@ -1,7 +1,7 @@
 /**
    [PROG]	      :  Scope of Variables in C
-   [AUTHOR]		  :  Saddam Arbaa
-   [Email]      : <saddamarbaas@gmail.com>
+   [AUTHOR]		   :  Saddam Arbaa
+   [Email]       : <saddamarbaas@gmail.com>
 
    Local , Global variables , Const , Automatic and Static   */
 
