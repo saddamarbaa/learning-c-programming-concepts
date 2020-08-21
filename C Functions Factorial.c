0e5d544306f82b08e61471c95ff6bf74ee6aaee1
saddamarbaa <51326421+saddamarbaa@users.noreply.github.com>
2020-08-21 19:29:23 +0700

    C  Function //  Factorial (N)  Program using Loop

--- C Functions Factorial.c
+++ C Functions Factorial.c
@@ -32,7 +32,7 @@ int Fact(int n)
 
     f = 1;   //initialize f by 1
 
-    for(i = n; i>= 1; i--)  // loop from given n to 1
+    for(i = n; i>= 1; i--)  // loop from given n to 1 
         f = f * i;        // calculate Factorial
     return f;             // return the  Factorial
 }/** End of fact */
