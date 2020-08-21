b5037093ab47d8f2791e73af544e2b7e8bf0e75a
saddamarbaa <51326421+saddamarbaa@users.noreply.github.com>
2020-08-21 20:16:09 +0700

    C Recursion // Recursively print number  from 1 to N

--- C Recursion Recursive Count.c
+++ C Recursion Recursive Count.c
@@ -1,1 +1,33 @@
+/**
+    [PROGRAM] : C  Recursion
+    [AUTHOR]  :   Saddam Arbaa
+    [Email]   :  <saddamarbaas@gmail.com>
+    Recursive Program to print numbers from N to 1 */
 
+#include <stdio.h>
+#include <stdlib.h>
+
+void Count_Recursively(int n); // Function declaration
+
+int main() // the main Function
+{
+    int n; //variable declaration
+
+    printf("Enter positive number N :"); // asking user input
+    scanf("%d",&n);
+    Count_Recursively(n);  // call the function
+
+   return 0;// signal to operating system everything works fine
+
+}/** End of main function */
+
+/**  Recursive function to print Numbers from one to N */
+
+void Count_Recursively(int n)
+{
+    if(n == 0) return ; /* Terminating condition(base case)*/
+       printf("%d\t", n);   // print the number
+
+     Count_Recursively(n - 1);  // Recursive call
+
+}/** End of Count_Recursively()*/
