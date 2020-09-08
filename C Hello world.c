09bc19bbfea935734c62bcd87479bd208a61c3f7
saddamarbaa <51326421+saddamarbaa@users.noreply.github.com>
2020-09-08 21:09:42 +0700

    Hello World!  C Program

--- C Hello world.c
+++ C Hello world.c
@@ -1,1 +1,21 @@
+/**
+    [PROGRAM] :  Hello world!
+    [AUTHOR]  :  Saddam Arbaa
+    [Email]   :  <saddamarbaas@gmail.com>
 
+    MY first Hello world C Program.*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[]) /* the river Code */
+{
+    // Print Hello World
+    printf("Hello world!\n");
+
+    printf("My name is Saddam Yahya\n");
+    printf("Am learning programming concept in C from(CS50x 2020) \n");
+
+    return 0;// signal to operating system everything works fine
+
+}/** End of main function */
